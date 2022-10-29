0ca28d73aadc4fbea2c4c3e5a94f8330a6c763f2
Quorra <923158428@qq.com>
2022-10-29 22:10:48 +0800

    :apple: 整合管理端

--- qhmall-product/src/main/java/com/qh/qhmall/product/controller/CategoryController.java
+++ qhmall-product/src/main/java/com/qh/qhmall/product/controller/CategoryController.java
@@ -1,20 +1,13 @@
 package com.qh.qhmall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.qh.common.utils.R;
 import com.qh.qhmall.product.entity.CategoryEntity;
 import com.qh.qhmall.product.service.CategoryService;
-import com.qh.common.utils.PageUtils;
-import com.qh.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 
@@ -32,14 +25,13 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装起来
      */
-    @RequestMapping("/list")
-    //@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    @GetMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> entities=categoryService.listWithTree();
 
-        return R.ok().put("page", page);
+        return R.ok().put("data", entities);
     }
 
 
--- qhmall-product/src/main/java/com/qh/qhmall/product/service/impl/CategoryServiceImpl.java
+++ qhmall-product/src/main/java/com/qh/qhmall/product/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,18 @@
 package com.qh.qhmall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.qh.common.utils.PageUtils;
 import com.qh.common.utils.Query;
-
 import com.qh.qhmall.product.dao.CategoryDao;
 import com.qh.qhmall.product.entity.CategoryEntity;
 import com.qh.qhmall.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -24,6 +26,51 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 查出所有分类以及子分类，以树形结构组装起来
+     *
+     * @return {@link List}<{@link CategoryEntity}>
+     */
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        //组装成父子的树形结构
+        //找到所有的一级分类
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map((menu) -> {
+            menu.setChildren(getChildren(menu, entities));
+            return menu;
+        }).sorted((menu1, menu2) -> {
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+        }).collect(Collectors.toList());
+        return level1Menus;
+    }
+
+    /**
+     * 递归查找所有菜单的子菜单
+     *
+     * @param root 根
+     * @param all  所有
+     * @return {@link List}<{@link CategoryEntity}>
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == root.getCatId();
+        }).map(categoryEntity -> {
+            //找到子菜单
+            categoryEntity.setChildren(getChildren(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((menu1, menu2) -> {
+            //菜单的排序
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return children;
     }
 
 }
